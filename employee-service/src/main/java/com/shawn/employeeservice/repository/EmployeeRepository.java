94d0885cbc19c4f40deb2397e0600fc9f7312771
unknown <shawnarun19@gmail.com>
2023-05-03 14:28:42 +0530

    Practice Project Micro Services

---
+++ employee-service/src/main/java/com/shawn/employeeservice/repository/EmployeeRepository.java
@@ -0,0 +1,36 @@
+package com.shawn.employeeservice.repository;
+
+import com.shawn.employeeservice.model.Employee;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class EmployeeRepository {
+
+    private List<Employee> employees = new ArrayList<>();
+
+    public Employee addEmployee(Employee employee){
+        employees.add(employee);
+        return employee;
+    }
+
+
+    public Employee findById(Long id){
+        return employees.stream()
+                .filter(a -> a.id().equals(id))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    public List<Employee> findAll(){
+        return employees;
+    }
+
+    public List<Employee> findByDepartmentId(Long departmentid){
+        return employees.stream()
+                .filter(a -> a.departmentId().equals(departmentid))
+                .toList();
+    }
+}
